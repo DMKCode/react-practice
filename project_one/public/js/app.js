0a9bc84f58d280631d9c24885593b13bca889bd4
David Kananda <davidkananda@gmail.com>
2017-08-24 13:56:03 +0100

    done basic

--- project_one/public/js/app.js
+++ project_one/public/js/app.js
@@ -1,7 +1,22 @@
-const Button = (props) => {
-    return (
-        <button>5</button>
-    );
-};
+class Button extends React.Component {
+    state = { 
+        counter: 0
+    }
+
+    handleClick = () => {
+        // this.setState({ counter: this.state.counter + 1 });
+        
+        // when updating the state using a value from the current state use other contract of setState method i.e. prevState
+        this.setState((prevState) => ({
+            counter: prevState.counter + 1
+        }))
+    }
+
+    render() {
+        return (
+            <button onClick={ this.handleClick }>{ this.state.counter }</button>
+        );
+    }
+}
 
 ReactDOM.render(<Button />, document.getElementById('content'));
