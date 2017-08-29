0d2b232af4acdb0456a4af1283d59ccfae4dbd7f
David Kananda <davidkananda@gmail.com>
2017-08-29 06:00:03 +0100

    module one complete

--- project_one/public/js/app.js
+++ project_one/public/js/app.js
@@ -1,22 +1,46 @@
 class Button extends React.Component {
-    state = { 
-        counter: 0
-    }
-
     handleClick = () => {
-        // this.setState({ counter: this.state.counter + 1 });
-        
-        // when updating the state using a value from the current state use other contract of setState method i.e. prevState
-        this.setState((prevState) => ({
-            counter: prevState.counter + 1
-        }))
+        this.props.handleClick(this.props.incrementValue)
     }
 
     render() {
         return (
-            <button onClick={ this.handleClick }>{ this.state.counter }</button>
+            <button onClick={ this.handleClick }>+{ this.props.incrementValue }</button>
         );
     }
 }
 
-ReactDOM.render(<Button />, document.getElementById('content'));
+const Result = (props) => {
+    return (
+        <div>{ props.counter }</div>
+    );
+}
+
+class App extends React.Component {
+    state = {
+        counter: 0
+    }
+
+    incrementCounter = (value) => {
+        // this.setState({ counter: this.state.counter + 1 });
+        
+        // when updating the state using a value from the current state use other contract of setState method i.e. prevState
+        this.setState((prevState) => ({
+            counter: prevState.counter + value
+        }))    
+    }
+    
+    render() {
+        return (
+            <div>
+                <Button incrementValue={ 1 } handleClick={ this.incrementCounter } />
+                <Button incrementValue={ 5 } handleClick={ this.incrementCounter } />
+                <Button incrementValue={ 10 } handleClick={ this.incrementCounter } />
+                <Button incrementValue={ 100 } handleClick={ this.incrementCounter } />
+                <Result counter={ this.state.counter } />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('content'));
