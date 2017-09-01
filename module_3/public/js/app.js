94b4c01f2e730358a8d31bc044213c4b8066d4fb
David Kananda <davidkananda@gmail.com>
2017-09-01 05:54:21 +0100

    module 3 global styles

--- module_3/public/js/app.js
+++ module_3/public/js/app.js
@@ -1,6 +1,6 @@
 const Stars = (props) => {
     return (
-        <div> 
+        <div className="col-5"> 
             <i className="fa fa-star"></i>
             <i className="fa fa-star"></i>
             <i className="fa fa-star"></i>
@@ -11,7 +11,7 @@ const Stars = (props) => {
 
 const Button = (props) => {
     return (
-        <div> 
+        <div className="col-2"> 
             <button>=</button>
         </div>
     );
@@ -19,20 +19,37 @@ const Button = (props) => {
 
 const Answer = (props) => {
     return (
-        <div> 
+        <div className="col-5"> 
             ...
         </div>
     );
 };
 
+const Numbers = (props) => {
+    return (
+        <div className="card text-center">
+            <div>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+            </div>
+        </div>
+    );
+}
+
 class Game extends React.Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <h3>Play Nine</h3>
-                <Stars />
-                <Button />
-                <Answer />
+                <hr/>
+                <div className="row">
+                    <Stars />
+                    <Button />
+                    <Answer />
+                </div>
+                <br/>
+                <Numbers />
             </div>
         );
     }
